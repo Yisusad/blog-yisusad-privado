964bf12bf2b6809dd7f74e6e81e1836479ace786
Jesús Acuña <jesuscheoz@gmail.com>
2025-01-21 17:51:29 -0300

    se modifca el modulo menu para poder listar en la vista solamente las categorias que contengas articulos

--- vistas/paginas/modulos/menu.php
+++ vistas/paginas/modulos/menu.php
@@ -8,13 +8,32 @@ MENU
 
 	<ul class="nav flex-column text-center">
 
-		<?php foreach ($categorias as $key => $value) :?>
-			
-			  <li class="nav-item">
-				<a class="nav-link text-white" href="<?php echo $blog["dominio"].$value["ruta_categoria"]; ?>"><?php echo $value["descripcion_categoria"]; ?></a>
-			  </li>
+	<?php 
+		$listaCategorias = array();
 
-		<?php endforeach ?>
+		// Recopilar categorías únicas
+		// Iterar sobre los artículos
+		foreach ($totalArticulos as $articulo) {
+			// Iterar sobre las categorías
+			foreach ($categorias as $categoria) {
+				// Si la categoría del artículo coincide con la categoría actual
+				if ($articulo["id_cat"] == $categoria["id_categoria"]) {
+					// Si la categoría no está en la lista, añadirla
+					$listaCategorias[$categoria["id_categoria"]] = $categoria;
+				}
+			}
+		}
+	?>
+
+	<?php foreach($listaCategorias as $categoria) :?>
+
+		<li class="nav-item">
+			<a class="nav-link text-white" href="<?php echo $blog["dominio"] . $categoria["ruta_categoria"]; ?>">
+				<?php echo $categoria["descripcion_categoria"]; ?>
+			</a>
+		</li>
+
+	<?php endforeach ?>
 
 	</ul>
 
