2b3c89754191ed80e907dfbd4fc7ff8a891fa8dd
Jesús Acuña <jesuscheoz@gmail.com>
2025-02-24 16:55:09 -0300

    Se termino seccion banner, ahora se pueden agregar banners, editarlos y eliminarlos

--- cms/app/Http/Controllers/BannerController.php
+++ cms/app/Http/Controllers/BannerController.php
@@ -37,8 +37,242 @@ class BannerController extends Controller
 
        $blog = Blog::all();
        $administradores = Administradores::all();
+       $banner = Banner::all()->unique('pagina_banner');
 
-       return view("paginas.banner", array("blog"=>$blog, "administradores"=>$administradores));
+       return view("paginas.banner", array("blog"=>$blog, "administradores"=>$administradores, "banner"=>$banner));
 
    }
+
+   /*=============================================
+    Crear un Banner
+    =============================================*/
+
+    public function store(Request $request)
+    {
+
+        $datos = array('pagina_banner' => $request->input("pagina_banner"),
+                       'titulo_banner' => $request->input("titulo_banner"),
+                       'descripcion_banner' => $request->input("descripcion_banner"),
+                       'imagen_temporal' => $request->file("img_banner"));
+        //Validar datos
+
+        if(!empty($datos)){
+
+            $validar = \Validator::make($datos, [
+                
+    			"titulo_banner"=> "required|regex:/^[0-9a-zA-ZñÑáéíóúÁÉÍÓÚ ]+$/i",
+    			"descripcion_banner"=> "required|regex:/^[0-9a-zA-ZñÑáéíóúÁÉÍÓÚ ]+$/i",
+    			"imagen_temporal"=> "required|image|mimes:jpg,jpeg,png|max:2000000"
+
+            ]);
+
+            //Guardar categoria
+            //Revisa si imagen_temporal no esta vacio y si la validacion es correcta
+            if(!$datos["imagen_temporal"] || $validar->fails()){
+
+                return redirect("banner")->with("no validacion", "");
+            }else{
+
+                //crear ruta para la imagen temporal
+                $aleatorio = uniqid();
+                $ruta = "img/banner/" . $aleatorio . "." . $datos["imagen_temporal"]->guessExtension();
+
+                //Redimensionar imagen
+
+                list($ancho, $alto) = getimagesize($datos["imagen_temporal"]);
+
+                $nuevoAncho = 1024;
+                $nuevoAlto = 576;
+
+                if ($datos["imagen_temporal"]->guessExtension() == "jpeg") {
+
+                    $origen = imagecreatefromjpeg($datos["imagen_temporal"]);
+                    $destino = imagecreatetruecolor($nuevoAncho, $nuevoAlto);
+                    imagecopyresized($destino, $origen, 0, 0, 0, 0, $nuevoAncho, $nuevoAlto, $ancho, $alto);
+                    imagejpeg($destino, $ruta);
+                }
+
+                if ($datos["imagen_temporal"]->guessExtension() == "png") {
+
+                    $origen = imagecreatefrompng($datos["imagen_temporal"]);
+                    $destino = imagecreatetruecolor($nuevoAncho, $nuevoAlto);
+                    imagealphablending($destino, FALSE);
+                    imagesavealpha($destino, TRUE);
+                    imagecopyresampled($destino, $origen, 0, 0, 0, 0, $nuevoAncho, $nuevoAlto, $ancho, $alto);
+                    imagepng($destino, $ruta);
+                }
+
+                //Guardar datos en la base de datos
+                $banner = new Banner();
+                $banner->pagina_banner = $datos["pagina_banner"];
+                $banner->titulo_banner = $datos["titulo_banner"];
+                $banner->descripcion_banner = $datos["descripcion_banner"];
+                $banner->img_banner = $ruta;
+
+                $banner->save();
+
+                return redirect("/banner")->with("ok-crear", "");
+            }
+
+        }else{
+
+            return redirect("/banner")->with("error", "");
+        }
+    }
+
+        //Editar un registro en la tabla categorias
+        public function show($id)
+        {
+    
+            $banner = Banner::where("id_banner", $id)->get();
+            $blog = Blog::all();
+            $administradores = Administradores::all();
+    
+            if(count($banner) != 0){
+    
+                return view("paginas.banner", array("status"=>200, "banner"=>$banner, "blog"=>$blog, "administradores"=>$administradores)); 
+            }
+    
+            else{
+                
+                return view("paginas.banner", array("status"=>404, "blog"=>$blog, "administradores"=>$administradores));
+    
+            }
+        }
+
+        //Actualizar un registro en la tabla banner
+      public function update($id, Request $request){
+          //Recogemos los datos del banner
+
+          $datos = array('pagina_banner' => $request->input("pagina_banner"),
+          'titulo_banner' => $request->input("titulo_banner"),
+          'descripcion_banner' => $request->input("descripcion_banner"),
+          'imagen_actual' => $request->input("imagen_actual"));
+
+          //Recoger imagen a guardar
+          $imagen = array('imagen_temporal' => $request->file("img_banner"));
+
+          //Validar datos
+
+          if(!empty($datos)){
+
+              $validar = \Validator::make($datos,[
+
+                  "titulo_banner"=> "required|regex:/^[0-9a-zA-ZñÑáéíóúÁÉÍÓÚ ]+$/i",
+    			        "descripcion_banner"=> "required|regex:/^[0-9a-zA-ZñÑáéíóúÁÉÍÓÚ ]+$/i",
+                  "imagen_actual"=> "required"
+
+              ]);
+
+              if($imagen["imagen_temporal"] != ""){
+
+                  $validarImagen = \Validator::make($imagen,[
+
+                  "imagen_temporal" => "required|image|mimes:jpg,jpeg,png|max:2000000"
+
+                  ]);
+
+                  if($validarImagen->fails()){
+
+                      return redirect("/banner")->with("no-validacion", "");
+
+                  }
+
+              }
+
+              if($validar->fails()){
+
+                  return redirect("/banner")->with("no-validacion", "");
+
+              }else{
+
+                  if($imagen["imagen_temporal"] != ""){
+
+                      //Eliminar imagen actual
+                      unlink($datos["imagen_actual"]);
+
+                      //crear ruta para la imagen temporal
+                      $aleatorio = uniqid();
+                      $ruta = "img/banner/" . $aleatorio . "." . $imagen["imagen_temporal"]->guessExtension();
+
+                      //Redimensionar imagen
+
+                      list($ancho, $alto) = getimagesize($imagen["imagen_temporal"]);
+
+                      $nuevoAncho = 1024;
+                      $nuevoAlto = 576;
+
+                      if ($imagen["imagen_temporal"]->guessExtension() == "jpeg") {
+
+                          $origen = imagecreatefromjpeg($imagen["imagen_temporal"]);
+                          $destino = imagecreatetruecolor($nuevoAncho, $nuevoAlto);
+                          imagecopyresized($destino, $origen, 0, 0, 0, 0, $nuevoAncho, $nuevoAlto, $ancho, $alto);
+                          imagejpeg($destino, $ruta);
+                      }
+
+                      if ($imagen["imagen_temporal"]->guessExtension() == "png") {
+
+                          $origen = imagecreatefrompng($imagen["imagen_temporal"]);
+                          $destino = imagecreatetruecolor($nuevoAncho, $nuevoAlto);
+                          imagealphablending($destino, FALSE);
+                          imagesavealpha($destino, TRUE);
+                          imagecopyresampled($destino, $origen, 0, 0, 0, 0, $nuevoAncho, $nuevoAlto, $ancho, $alto);
+                          imagepng($destino, $ruta);
+                      }
+
+                  }else{
+                      
+                      $ruta = $datos["imagen_actual"];
+                  }
+
+                  //Actualizar datos en la base de datos
+
+                  $datos = array("pagina_banner" => $datos["pagina_banner"],
+                              "titulo_banner" => $datos["titulo_banner"],
+                                "descripcion_banner" => $datos["descripcion_banner"],
+                                "img_banner" => $ruta
+                              );
+                  //Actualizar datos en la base de datos
+                  $banner = Banner::where("id_banner", $id)->update($datos);
+
+                  return redirect("/banner")->with("ok-editar", "");
+
+              }
+
+          }else{
+
+              return redirect("/banner")->with("error", "");
+
+          }
+
+
+      }
+
+       // Eliminar un registro en la tabla Banner
+
+    public function destroy($id, Request $request){
+
+    	$paraEliminar =Banner::where("id_banner", $id)->get();
+    	
+    	if(!empty($paraEliminar)){
+
+    		if(!empty($paraEliminar[0]["img_banner"])){
+
+    			unlink($paraEliminar[0]["img_banner"]);
+    		
+    		}
+
+            //Aqui se borra la categoria
+            $banner = Banner::where("id_banner", $paraEliminar[0]["id_banner"])->delete();
+    		//Responder al AJAX de JS para Sweet Alert
+    		return "ok";
+    	
+    	}else{
+
+    		return redirect("/banner")->with("no-borrar", "");
+    	
+
+    	}
+
+    }
 }
--- cms/resources/views/paginas/banner.blade.php
+++ cms/resources/views/paginas/banner.blade.php
@@ -93,4 +93,277 @@
   <!-- /.content -->
 </div>
 
+
+<!--=====================================
+Crear Banner
+======================================-->
+
+<div class="modal" id="crearBanner">
+  <div class="modal-dialog modal-lg">
+
+    <div class="modal-content">
+
+      <form action="{{url('/')}}/banner" method="post" enctype="multipart/form-data">
+
+        @csrf
+
+        <div class="modal-header bg-info">
+          <h4 class="modal-title">Crear Banner</h4>
+          <button type="button" class="close" data-dismiss="modal">&times;</button>
+        </div>
+
+        <!-- Modal body -->
+        <div class="modal-body">
+
+           {{-- Página --}}
+           
+          <div class="input-group mb-3">
+
+            <div class="input-group-append input-group-text">
+              <i class="fas fa-list-ul"></i>
+            </div>
+
+            <select class="form-control"  name="pagina_banner" required>
+
+              <option value="" hidden>Elige Pagina</option>
+             
+              @foreach ($banner as $key => $value)
+
+                <option value="{{$value->pagina_banner}}">{{$value->pagina_banner}}</option>              
+
+              @endforeach
+
+            </select>
+            
+          </div>
+        
+          {{-- Título banner --}}
+                    
+          <div class="input-group mb-3">
+
+            <div class="input-group-append input-group-text">
+              <i class="fas fa-list-ul"></i>
+            </div>
+
+            <input type="text" class="form-control" name="titulo_banner" placeholder="Ingrese el titulo del banner" value="{{old("titulo_banner")}}" required> 
+
+          </div> 
+
+          {{-- Descripción banner --}}
+                  
+          <div class="input-group mb-3">
+     
+            <div class="input-group-append input-group-text">
+              <i class="fas fa-pencil-alt"></i>
+            </div>
+
+            <input type="text" class="form-control" name="descripcion_banner" placeholder="Ingrese la descripción del banner" value="{{old("descripcion_banner")}}" maxlength="220" required> 
+
+          </div> 
+
+          <hr class="pb-2">
+
+          <div class="form-group my-2 text-center">
+
+            <div class="btn btn-default btn-file">
+
+                <i class="fas fa-paperclip"></i> Adjuntar Imagen del banner
+
+                <input type="file" name="img_banner" required>
+               
+            </div>
+
+            <img class="previsualizarImg_img_banner img-fluid py-2">
+
+             <p class="help-block small">Dimensiones: 680px * 400px | Peso Max. 2MB | Formato: JPG o PNG</p>
+
+          </div>
+
+        </div>
+
+        <!-- Modal footer -->
+        <div class="modal-footer d-flex justify-content-between">
+
+          <div>
+            <button type="button" class="btn btn-danger" data-dismiss="modal">Cerrar</button>
+          </div>
+
+          <div>
+            <button type="submit" class="btn btn-primary">Guardar</button>
+          </div>
+
+        </div>
+
+      </form>
+
+    </div>
+
+  </div>
+
+</div>
+
+<!-- MODAL PARA EDITAR Categorías -->
+@if (isset($status))
+  @if ($status == 200)
+
+    @foreach ($banner as $key => $value)
+      <div class="modal" id="editarBanner">
+  
+        <div class="modal-dialog">
+        
+          <div class="modal-content">
+      
+            <form method="POST" action="{{url('/')}}/banner/{{$value->id_banner}}" enctype="multipart/form-data">
+              @method('PUT')
+              @csrf
+            
+              <div class="modal-header bg-info">
+                
+                <h4 class="modal-title">Editar Banner</h4>
+                <button type="button" class="close" data-dismiss="modal">&times;</button>
+      
+              </div>
+      
+              <div class="modal-body">
+
+                
+               {{-- Pagina banner --}}
+               
+              <div class="input-group mb-3">
+
+                <div class="input-group-append input-group-text">
+                  <i class="fas fa-list-ul"></i>
+                </div>
+
+                <select class="form-control"  name="pagina_banner" required>
+               
+                  @foreach ($banner as $key => $value)
+
+                    <option value="{{$value->pagina_banner}}">{{$value->pagina_banner}}</option>              
+  
+                  @endforeach
+
+
+                </select>
+                
+              </div>
+      
+                  {{-- Titulo de banner --}}
+      
+                  <div class="input-group mb-3">
+                    
+                    <div class="input-group-append input-group-text">               
+                      <i class="fas fa-list-ul"></i>
+                    </div>
+      
+                    <input type="text" class="form-control" name="titulo_banner" placeholder="Ingrese título del banner" value="{{ $value->titulo_banner }}" required>
+      
+                  </div>
+      
+                  {{-- Descripción de banner --}}
+      
+                  <div class="input-group mb-3">
+                    
+                    <div class="input-group-append input-group-text">               
+                      <i class="fas fa-pencil-alt"></i>
+                    </div>
+      
+                    <input type="text" class="form-control" name="descripcion_banner" placeholder="Ingrese descripción del banner" value="{{ $value->descripcion_banner }}" maxlength="30" required>
+      
+                  </div>
+
+  
+      
+                  <hr class="pb-2">
+      
+                  {{-- Imagen de Banner --}}
+      
+                  <div class="form-group my-2 text-center">
+                    
+                    <div class="btn btn-default btn-file">               
+                      <i class="fas fa-paperclip"></i>Agregar Imagen
+                      <input type="file" name="img_banner">
+                    </div>
+                    
+                    <img src="{{url('/')}}/{{$value->img_banner}}" class="previsualizarImg_img_banner img-fluid py-2">
+
+                    <input type="hidden" value="{{$value->img_banner}}" name="imagen_actual">
+
+                    <p class="help-block small">Dimensiones: 1024px * 576px | Peso Max. 2MB | Formato: JPG o PNG</p>
+      
+                  </div>        
+      
+              </div>
+      
+              <div class="modal-footer d-flex">
+                
+                <div>
+                  <button type="button" class="btn btn-danger" data-dismiss="modal">Cerrar</button>
+                </div>
+      
+                <div>
+                  <button type="submit" class="btn btn-primary">Guardar</button>
+                </div>
+      
+              </div>
+      
+            </form>
+      
+          </div> 
+      
+        </div> 
+      
+      </div>    
+    @endforeach
+
+    <script>
+      $('#editarBanner').modal();
+    </script>
+
+  @endif
+
+@endif
+
+
+<!-- Notie Js Notificaciones comienzo -->
+@if (Session::has("ok-crear"))
+
+<script>
+    notie.alert({ type: 1, text: '¡El banner ha sido creada correctamente!', time: 10 })
+</script>
+
+@endif
+
+@if (Session::has("no-validacion"))
+
+<script>
+    notie.alert({ type: 2, text: '¡Hay campos no válidos en el formulario!', time: 10 })
+</script>
+
+@endif
+
+@if (Session::has("error"))
+
+  <script>
+      notie.alert({ type: 3, text: '¡Error en el gestor de banners!', time: 10 })
+ </script>
+
+@endif
+
+@if (Session::has("ok-editar"))
+
+  <script>
+      notie.alert({ type: 1, text: '¡El banner ha sido actualizado correctamente!', time: 10 })
+ </script>
+@endif
+
+@if (Session::has("no-borrar"))
+
+  <script>
+      notie.alert({ type: 1, text: '¡Error al borrar el banner', time: 10 })
+ </script>
+
+@endif
+<!-- Notie Js Notificaciones fin -->
+
 @endsection
