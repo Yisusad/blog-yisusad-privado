93264f422fcbaec1747d340534ad8499abc21310
Jesús Acuña <jesuscheoz@gmail.com>
2025-02-24 23:27:51 -0300

    Se termino de crear la seccion de anuncios, ahora se pueden, crear, editar y eliminar anuncios

--- cms/app/Http/Controllers/AnunciosController.php
+++ cms/app/Http/Controllers/AnunciosController.php
@@ -62,8 +62,130 @@ class AnunciosController extends Controller
 
 		$blog = Blog::all();
 		$administradores = Administradores::all();
+    $anuncio = Anuncios::all()->unique('pagina_anuncio');
 
-		return view("paginas.anuncios", array("blog"=>$blog, "administradores"=>$administradores));
+		return view("paginas.anuncios", array("blog"=>$blog, "administradores"=>$administradores, "anuncio"=>$anuncio));
 
 	}
+
+    //Crear nuevo registro en la tabla Anuncios
+
+    public function store(Request $request){
+
+        //Recoger datos
+        $datos = array('pagina_anuncio' => $request->input("pagina_anuncio"),
+                       'codigo_anuncio' => $request->input("codigo_anuncio"));
+
+        //Validar datos
+        if(!empty($datos)){
+
+            $validar = \Validator::make($datos, [
+                "codigo_anuncio" => 'required|regex:/^[(\\)\\=\\&\\$\\;\\-\\_\\*\\"\\<\\>\\?\\¿\\!\\¡\\:\\,\\.\\0-9a-zA-ZñÑáéíóúÁÉÍÓÚ ]+$/i',
+            ]);
+
+      
+            if($validar->fails()){
+                var_dump("Putas");
+                exit();
+                return redirect("/anuncios")->with("no validacion", "");
+            }else{
+
+                //Guardar datos en la base de datos
+                $anuncio = new Anuncios();
+                $anuncio->pagina_anuncio = $datos["pagina_anuncio"];
+                $anuncio->codigo_anuncio = $datos["codigo_anuncio"];
+
+                $anuncio->save();
+                
+                return redirect("/anuncios")->with("ok-crear", "");
+            }
+
+        }else{
+
+            return redirect("/anuncios")->with("error", "");
+        }
+    }
+
+    
+    //Editar un registro en la tabla anuncios
+    public function show($id)
+    {
+
+        $anuncio = Anuncios::where("id_anuncio", $id)->get();
+        $blog = Blog::all();
+        $administradores = Administradores::all();
+
+        if(count($anuncio) != 0){
+
+            return view("paginas.anuncios", array("status"=>200, "anuncio"=>$anuncio, "blog"=>$blog, "administradores"=>$administradores)); 
+        }
+
+        else{
+            
+            return view("paginas.anuncios", array("status"=>404, "blog"=>$blog, "administradores"=>$administradores));
+
+        }
+    }
+
+      //Actualizar un registro en la tabla Anuncios
+      public function update($id, Request $request)
+      {
+          //Recogemos los datos de la categoria
+          //Recoger datos
+          $datos = array('pagina_anuncio' => $request->input("pagina_anuncio"),
+                          'codigo_anuncio' => $request->input("codigo_anuncio"));
+  
+          //Validar datos
+  
+          if(!empty($datos)){
+  
+              $validar = \Validator::make($datos, [
+                "codigo_anuncio" => 'required|regex:/^[(\\)\\=\\&\\$\\;\\-\\_\\*\\"\\<\\>\\?\\¿\\!\\¡\\:\\,\\.\\0-9a-zA-ZñÑáéíóúÁÉÍÓÚ ]+$/i',
+              ]);
+  
+              if($validar->fails()){
+  
+                  return redirect("/anuncios")->with("no-validacion", "");
+  
+              }else{
+                  //Actualizar datos en la base de datos
+  
+                  $datos = array("pagina_anuncio" => $datos["pagina_anuncio"],
+                                 "codigo_anuncio" => $datos["codigo_anuncio"]
+                              );
+                  //Actualizar datos en la base de datos
+                  $anuncio = Anuncios::where("id_anuncio", $id)->update($datos);
+  
+                  return redirect("/anuncios")->with("ok-editar", "");
+  
+              }
+  
+          }else{
+  
+              return redirect("/anuncios")->with("error", "");
+  
+          }
+  
+  
+      }
+
+
+      // Eliminar un registro en la tabla categorias
+      public function destroy($id, Request $request){
+
+        $paraEliminar =Anuncios::where("id_anuncio", $id)->get();
+        
+        if(!empty($paraEliminar)){
+          //Aqui se borra el anuncio
+          $anuncio = Anuncios::where("id_anuncio", $paraEliminar[0]["id_anuncio"])->delete();
+          //Responder al AJAX de JS para Sweet Alert
+          return "ok";
+        
+        }else{
+
+          return redirect("/anuncios")->with("no-borrar", "");
+        
+        }
+
+      }
 }
--- cms/resources/views/paginas/anuncios.blade.php
+++ cms/resources/views/paginas/anuncios.blade.php
@@ -89,3 +89,213 @@
     </div>
     <!-- /.content -->
   </div>
+
+  <!--=====================================
+Crear Banner
+======================================-->
+
+<div class="modal" id="crearAnuncio">
+  <div class="modal-dialog modal-lg">
+
+    <div class="modal-content">
+
+      <form action="{{url('/')}}/anuncios" method="post" enctype="multipart/form-data">
+
+        @csrf
+
+        <div class="modal-header bg-info">
+          <h4 class="modal-title">Crear Anuncio</h4>
+          <button type="button" class="close" data-dismiss="modal">&times;</button>
+        </div>
+
+        <!-- Modal body -->
+        <div class="modal-body">
+
+           {{-- Página --}}
+           
+          <div class="input-group mb-3">
+
+            <div class="input-group-append input-group-text">
+              <i class="fas fa-list-ul"></i>
+            </div>
+
+            <select class="form-control"  name="pagina_anuncio" required>
+
+              <option value="" hidden>Elige Pagina</option>
+             
+              @foreach ($anuncio as $key => $value)
+
+                <option value="{{$value->pagina_anuncio}}">{{$value->pagina_anuncio}}</option>              
+
+              @endforeach
+
+            </select>
+            
+          </div>
+          
+           {{-- Codigo Anuncio --}}
+      
+           <div class="input-group mb-3">
+                    
+            <div class="input-group-append input-group-text">               
+              <i class="fas fa-pencil-alt"></i>
+            </div>
+
+            <input type="text" class="form-control" name="codigo_anuncio" placeholder="Ingrese el codigo html del anuncio" value="{{ $value->codigo_anuncio }}"  required>
+
+          </div>
+
+        </div>
+
+        <!-- Modal footer -->
+        <div class="modal-footer d-flex justify-content-between">
+
+          <div>
+            <button type="button" class="btn btn-danger" data-dismiss="modal">Cerrar</button>
+          </div>
+
+          <div>
+            <button type="submit" class="btn btn-primary">Guardar</button>
+          </div>
+
+        </div>
+
+      </form>
+
+    </div>
+
+  </div>
+
+</div>
+
+
+<!-- MODAL PARA EDITAR ANUNCIOS -->
+@if (isset($status))
+  @if ($status == 200)
+
+    @foreach ($anuncio as $key => $value)
+      <div class="modal" id="editarAnuncio">
+  
+        <div class="modal-dialog">
+        
+          <div class="modal-content">
+      
+            <form method="POST" action="{{url('/')}}/anuncios/{{$value->id_anuncio}}" enctype="multipart/form-data">
+              @method('PUT')
+              @csrf
+            
+              <div class="modal-header bg-info">
+                
+                <h4 class="modal-title">Editar Anuncio</h4>
+                <button type="button" class="close" data-dismiss="modal">&times;</button>
+      
+              </div>
+      
+              <div class="modal-body">    
+            
+                {{-- Pagina Anuncio --}}
+                    
+                <div class="input-group mb-3">
+
+                  <div class="input-group-append input-group-text">
+                    <i class="fas fa-list-ul"></i>
+                  </div>
+
+                  <select class="form-control"  name="pagina_anuncio" required>
+                
+                    @foreach ($anuncio as $key => $value)
+
+                      <option value="{{$value->pagina_anuncio}}">{{$value->pagina_anuncio}}</option>              
+    
+                    @endforeach
+
+                  </select>
+                  
+                </div>  
+                
+              {{-- Descripción artículo --}}
+                      
+                  <div class="input-group mb-3">
+            
+                    <div class="input-group-append input-group-text">
+                      <i class="fas fa-pencil-alt"></i>
+                    </div>
+
+                    <input type="text" class="form-control" name="codigo_anuncio" placeholder="Ingrese el codigo html del anuncio" value="{{$value->codigo_anuncio}}" maxlength="220" required> 
+
+                  </div> 
+                       
+      
+              </div>
+      
+              <div class="modal-footer d-flex">
+                
+                <div>
+                  <button type="button" class="btn btn-danger" data-dismiss="modal">Cerrar</button>
+                </div>
+      
+                <div>
+                  <button type="submit" class="btn btn-primary">Guardar</button>
+                </div>
+      
+              </div>
+      
+            </form>
+      
+          </div> 
+      
+        </div> 
+      
+      </div>    
+    @endforeach
+
+    <script>
+      $('#editarAnuncio').modal();
+    </script>
+
+  @endif
+
+@endif
+
+<!-- Notie Js Notificaciones inicio -->
+@if (Session::has("ok-crear"))
+
+<script>
+    notie.alert({ type: 1, text: '¡El Anuncio ha sido creada correctamente!', time: 10 })
+</script>
+
+@endif
+
+@if (Session::has("no-validacion"))
+
+<script>
+    notie.alert({ type: 2, text: '¡Hay campos no válidos en el formulario!', time: 10 })
+</script>
+
+@endif
+
+@if (Session::has("error"))
+
+  <script>
+      notie.alert({ type: 3, text: '¡Error en el gestor de Anuncios!', time: 10 })
+ </script>
+
+@endif
+
+@if (Session::has("ok-editar"))
+
+  <script>
+      notie.alert({ type: 1, text: '¡El Anuncio ha sido actualizado correctamente!', time: 10 })
+ </script>
+@endif
+
+@if (Session::has("no-borrar"))
+
+  <script>
+      notie.alert({ type: 1, text: '¡Error al borrar el Anuncio', time: 10 })
+ </script>
+
+@endif
+<!-- Notie Js Notificaciones fin -->
+
+@endsection
